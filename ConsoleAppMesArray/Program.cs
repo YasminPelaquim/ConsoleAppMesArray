8e40b25506ab26e6ab4d812b61171c02263e3410
Usuario <Usuario@CPD02-M06>
2024-04-30 11:07:04 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppMesArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppMesArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] mes = { "", "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro" };
+
+            Console.WriteLine("Digite o número do mês: ");
+            int nu = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("O mês escolhido foi " + mes[nu]);
+
+
+
+            Console.ReadKey();
+
+            String[] mees = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro" };
+
+            Console.WriteLine("Digite o número do mês: ");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("O mês escolhido foi " + mees[num - 1]);
+
+            Console.ReadKey();
+        }
+    }
+}
+ 
